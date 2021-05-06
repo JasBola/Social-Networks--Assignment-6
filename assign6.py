174707becdf185b09a2ffe5fea5d13635bed3732
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-05-05 17:24:49 -0700

    Update assign6.py

--- assign6.py
+++ assign6.py
@@ -1,14 +1,9 @@
 import networkx as nx 
-import pandas as pd 
-
-
-
 
 f = open("amazon-meta.txt", "r")
 Data=f.readline()
 g=nx.Graph()
 for ASIN in Data:
-    for similar in Data:
+    while ASIN in similar:
         g.add_edge(ASIN, similar)
-    
-print(nx.info(G))
+print(nx.info(g))
