367be932561a3909cc888c1ac3a0e2f76667cd76
JasBola <jbola1@csustan.edu>
2021-05-06 16:49:55 -0700

    Delete assign6.py

--- assign6.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx 
-
-f = open("amazon-meta.txt", "r")
-Data=f.readline()
-g=nx.Graph()
-for ASIN in Data:
-    while ASIN in similar:
-        g.add_edge(ASIN, similar)
-print(nx.info(g))
